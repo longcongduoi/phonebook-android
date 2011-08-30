a9a0a4bed5026537443af388caf234e7ff092517
umeshk <umeshk@dc3346d9-979f-471d-ace4-0bf8e23cca8f>
2011-08-30 11:03:04 +0000

    added pic size to log message
    git-svn-id: http://10.9.8.35/svn/nbos/trunk/android/phonebook-android@428 dc3346d9-979f-471d-ace4-0bf8e23cca8f

--- src/com/nbos/phonebook/sync/client/ServerData.java
+++ src/com/nbos/phonebook/sync/client/ServerData.java
@@ -6,7 +6,7 @@ public class ServerData {
 	static String tag = "ServerData";
 	
 	public ServerData(String contactId, String serverId, String picId, String picSize, String picHash) {
-		Log.i(tag, "contactId: "+contactId+", serverId: "+serverId+", picId: "+picId+", picHash: "+picHash);
+		Log.i(tag, "contactId: "+contactId+", serverId: "+serverId+", picId: "+picId+", size: "+picSize+", picHash: "+picHash);
 		this.contactId = contactId;
 		this.serverId = serverId;
 		this.picId = picId;
